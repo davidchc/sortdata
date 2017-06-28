ac4ca534c0c81cfbe21f33d9766d6c29416381a6
David CHC <david.chc@gmail.com>
2017-06-27 21:06:27 -0300

    formatacao PSR2

--- src/SortData/SortData.php
+++ src/SortData/SortData.php
@@ -4,63 +4,61 @@ namespace SortData;
 
 class SortData
 {
-	private $sorts;
-	private $source;
-	private $field;
-	private $data;
-	private $isRand;
+    private $sorts;
+    private $source;
+    private $field;
+    private $data;
+    private $isRand;
 
-	public function __construct(array $sorts, array $source, $field='name') 
-	{
-		$this->sorts = $sorts;
-		$this->source = $source;
-		$this->field = $field;
-		$this->data = [];
-		$this->isRand = false;		
-	}
+    public function __construct(array $sorts, array $source, $field='name')
+    {
+        $this->sorts = $sorts;
+        $this->source = $source;
+        $this->field = $field;
+        $this->data = [];
+        $this->isRand = false;
+    }
 
-	public function rand()
-	{
-		$this->isRand = true;
-		return $this;
-	}
+    public function rand()
+    {
+        $this->isRand = true;
+        return $this;
+    }
 
-	public function sort()
-	{
-		foreach($this->source as $key => $value) {
-			foreach($this->sorts as $i => $search) {
-				if(preg_match("/$search/is", $value[$this->field])) {
-					$this->addData($i, $value);
-					unset($this->source[$key]);
-				}
-			}
-		}
-		return $this;	
-	}
+    public function sort()
+    {
+        foreach ($this->source as $key => $value) {
+            foreach ($this->sorts as $i => $search) {
+                if (preg_match("/$search/is", $value[$this->field])) {
+                    $this->addData($i, $value);
+                    unset($this->source[$key]);
+                }
+            }
+        }
+        return $this;
+    }
 
-	protected function addData($index, $value)
-	{
-		$this->data[$index][] = $value;
-	}	
-	
-	public function getDataSort()
-	{
-    	$result = [];
-      	foreach($this->data as $d) {
-      		$result = array_merge($result, $d);
-      	}
+    protected function addData($index, $value)
+    {
+        $this->data[$index][] = $value;
+    }
+    
+    public function getDataSort()
+    {
+        $result = [];
+        foreach ($this->data as $d) {
+            $result = array_merge($result, $d);
+        }
       
-      	if ($this->isRand) {
-        	shuffle($result);
-      	}
+        if ($this->isRand) {
+            shuffle($result);
+        }
      
-      	return $result;
-	}
+        return $result;
+    }
 
-	public function getResults()
-	{
-		
-		return array_merge($this->getDataSort(), $this->source);
-	}
-
+    public function getResults()
+    {
+        return array_merge($this->getDataSort(), $this->source);
+    }
 }
